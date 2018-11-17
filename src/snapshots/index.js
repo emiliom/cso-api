6c342b3b039c897c6025f38fa0ab9360ca1a5c71
Jonah Joughin <jonah@jough.in>
2018-11-18 01:19:25 +0400

    Updated snapshots

--- src/snapshots/index.js
+++ src/snapshots/index.js
@@ -2,5 +2,5 @@ const request = require("request");
 const fs = require("fs")
 
 fs.mkdirSync("dist")
-request('https://api.communitysnowobs.org/obs?format=geojson&limit=10000').pipe(fs.createWriteStream('dist/observations-geojson'))
-request('https://api.communitysnowobs.org/obs?format=csv&limit=10000').pipe(fs.createWriteStream('dist/observations-csv'))
+request('https://api.communitysnowobs.org/obs?format=geojson&limit=10000').pipe(fs.createWriteStream('dist/observations.geojson'))
+request('https://api.communitysnowobs.org/obs?format=csv&limit=10000').pipe(fs.createWriteStream('dist/observations.csv'))
