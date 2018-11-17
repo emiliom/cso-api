48f5dede11d5e59ed7de56830f63653932d1a6b8
Jonah Joughin <jonah@jough.in>
2018-11-18 01:15:21 +0400

    Moved snapshots to script

---
+++ src/snapshots/index.js
@@ -0,0 +1,6 @@
+const request = require("request");
+const fs = require("fs")
+
+fs.mkdirSync("dist")
+request('https://api.communitysnowobs.org/obs>format=geojson&limit=10000').pipe(fs.createWriteStream('dist/observations.geojson'))
+request('https://api.communitysnowobs.org/obs?format=csv&limit=10000').pipe(fs.createWriteStream('dist/observations.csv'))
