b11f512c8b7c46ae8e27bf0352b1646369a5df42
Jonah Joughin <jonah@jough.in>
2018-11-18 22:40:41 +0400

    Updated now.json

--- src/import/index.js
+++ src/import/index.js
@@ -3,6 +3,8 @@ const { retrieveObservations, insertObservations } = require('./observations')
 
 module.exports = (req, res) => {
   const providers = [mh]
+  res.end("Test")
+  return
   retrieveObservations(providers)
     .then(data => insertObservations(data))
     .then(results => res.end(results))
