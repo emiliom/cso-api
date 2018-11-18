c6b5ac14c1e08374b0dcadc211b39047e60ae121
Jonah Joughin <jonah@jough.in>
2018-11-18 23:24:55 +0400

    Updated import

--- src/import/utils.js
+++ src/import/utils.js
@@ -20,7 +20,8 @@ const __withElevationBatch = async function(data) {
     key: process.env.ELEVATION_API_KEY
   }
   const response = await axios.get(BASE_ELEVATION_URL, {params: params});
-  console.log(response)
+  console.log(response.data)
+  console.log(response.data.results)
   const results = data.map((x,i) => (Object.assign(x, {elevation: response.data.results[i].elevation})))
   return results
 }
