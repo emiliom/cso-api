934f740f3215b0f6f1f73e76486619ee693aed64
Jonah Joughin <jonah@jough.in>
2018-11-18 15:14:23 +0400

    Updated observations

--- src/snapshots/index.js
+++ src/snapshots/index.js
@@ -1,9 +1,14 @@
 const request = require("request");
+const observations = require("./__observations.js")
 const fs = require("fs")
 
 fs.mkdirSync("dist")
 request('https://api.communitysnowobs.org/obs?format=geojson&limit=10000').pipe(fs.createWriteStream('dist/observations.geojson'))
-// request('https://api.communitysnowobs.org/obs?format=csv&limit=10000').pipe(fs.createWriteStream('dist/observations.csv'))
+//request('https://api.communitysnowobs.org/obs?format=csv&limit=10000').pipe(fs.createWriteStream('dist/observations.csv'))
 
-
-request('https://api.zeit.co/v3/now/deployments').pipe(fs.createWriteStream('dist/observations.csv'))
+observations({limit: 'ALL', format: 'csv'})
+  .then(results => {
+    const stream = fs.createWriteStream('dist/observations.csv');
+    stream.write(results);
+    stream.end();
+  })
