bb41253e9ba36efe40850a998aa576fdd696dc14
Jonah Joughin <jonah@jough.in>
2018-11-18 23:09:39 +0400

    Updated import

--- src/import/observations.js
+++ src/import/observations.js
@@ -14,21 +14,21 @@ const __retrieveObservation = async function(provider) {
   return data
 }
 
-// const pgConfig = {
-//   max: 1,
-//   user: process.env.SQL_USERNAME,
-//   password: process.env.SQL_PASSWORD,
-//   host: process.env.SQL_HOSTNAME,
-//   database: process.env.SQL_DATABASE,
-// }
-//
-// const query = `
-//   INSERT INTO observations(location, id, author, depth, timestamp, source, elevation)
-//   VALUES(ST_SetSRID(ST_MakePoint($1,$2), 4326), $3, $4, $5, $6, $7, $8)
-//   ON CONFLICT DO NOTHING
-// `
-//
-// let pgPool;
+const pgConfig = {
+  max: 1,
+  user: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD,
+  host: process.env.SQL_HOSTNAME,
+  database: process.env.SQL_DATABASE,
+}
+
+const query = `
+  INSERT INTO observations(location, id, author, depth, timestamp, source, elevation)
+  VALUES(ST_SetSRID(ST_MakePoint($1,$2), 4326), $3, $4, $5, $6, $7, $8)
+  ON CONFLICT DO NOTHING
+`
+
+let pgPool;
 
 // const insertObservations = async function (observations) {
 //   if (!pgPool) {
