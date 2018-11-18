8c675d823c42cad63de6df7002a3c4d87cd4ce93
Jonah Joughin <jonah@jough.in>
2018-11-18 14:58:57 +0400

    Added __observations

--- src/snapshots/index.js
+++ src/snapshots/index.js
@@ -1,12 +1,12 @@
 const request = require("request");
-const { __observations } = require("../observations")
+const observations = require("../__observations.js")
 const fs = require("fs")
 
 fs.mkdirSync("dist")
 request('https://api.communitysnowobs.org/obs?format=geojson&limit=10000').pipe(fs.createWriteStream('dist/observations.geojson'))
 //request('https://api.communitysnowobs.org/obs?format=csv&limit=10000').pipe(fs.createWriteStream('dist/observations.csv'))
 
-__observations({limit: 'ALL', format: 'csv'})
+observations({limit: 'ALL', format: 'csv'})
   .then(results => {
     const stream = fs.createWriteStream('dist/observations.csv');
     stream.write(results);
