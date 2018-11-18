2d807fe4bdcd1e1dffc6c303b91af843b3950c0c
Jonah Joughin <jonah@jough.in>
2018-11-18 22:17:59 +0400

    Updated import

--- src/import/index.js
+++ src/import/index.js
@@ -3,7 +3,7 @@ const mh = require('./mountainhub')
 const { retrieveObservations, insertObservations } = require('./observations')
 
 module.exports = (req, res) => {
-  providers = [mh]
+  const providers = [mh]
   retrieveObservations(providers)
     .then(data => insertObservations(data))
     .then(results => res.end(results))
