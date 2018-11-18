bdb887f694e58b6df21fa164da3e12f28911987e
Jonah Joughin <jonah@jough.in>
2018-11-18 15:07:46 +0400

    Removed __observations.js

--- src/snapshots/index.js
+++ src/snapshots/index.js
@@ -1,16 +1,9 @@
-const pg = require('pg');
-
 const request = require("request");
-const observations = require("../__observations.js")
 const fs = require("fs")
 
 fs.mkdirSync("dist")
 request('https://api.communitysnowobs.org/obs?format=geojson&limit=10000').pipe(fs.createWriteStream('dist/observations.geojson'))
-//request('https://api.communitysnowobs.org/obs?format=csv&limit=10000').pipe(fs.createWriteStream('dist/observations.csv'))
+// request('https://api.communitysnowobs.org/obs?format=csv&limit=10000').pipe(fs.createWriteStream('dist/observations.csv'))
 
-observations({limit: 'ALL', format: 'csv'})
-  .then(results => {
-    const stream = fs.createWriteStream('dist/observations.csv');
-    stream.write(results);
-    stream.end();
-  })
+
+request('https://api.zeit.co/v3/now/deployments').pipe(fs.createWriteStream('dist/observations.csv'))
