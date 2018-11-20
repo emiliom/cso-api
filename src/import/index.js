be981c3584c7d0be18cbff6c80759fd62574e031
Jonah Joughin <jonah@jough.in>
2018-11-20 11:34:09 +0400

    Added RegObs

--- src/import/index.js
+++ src/import/index.js
@@ -1,10 +1,11 @@
 const MountainHub = require('./providers/mountainhub')
 const SnowPilot = require('./providers/snowpilot')
+const RegOs = require('./providers/regobs')
 const retrieveObservations = require('./retrieveObservations')
 const insertObservations = require('./insertObservations')
 
 module.exports = (req, res) => {
-  const providers = [MountainHub, SnowPilot]
+  const providers = [MountainHub, SnowPilot, RegObs]
   retrieveObservations(providers)
     .then(data => insertObservations(data))
     .then(results => res.end(results))
---
+++ src/import/providers/regobs.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const { generateId } = require("../utils")
+
+const BASE_URL = 'https://api.nve.no/hydrology/regobs/webapi_latest/search/all';
+const HEADER = { "Content-Type": "application/json" };
+
+const rawData = async function(min_timestamp, max_timestamp) {
+
+  const args = {
+    FromDate : new Date(min_timestamp).toISOString().split("T")[0]
+    ToDate : new Date(max_timestamp).toISOString().split("T")[0]
+  };
+
+  try {
+    let offset = 0;
+    let results = []
+    while (true) {
+      const response = await axios.get(BASE_URL, {params: {...args, offset}, headers: HEADER});
+      results = results.concat(response.data.Results)
+      offset += response.data.ResultsInPage
+      if (offset >= response.data.TotalMatches) {
+        break
+      }
+    }
+    return results
+  } catch (error) {
+    return error;
+  }
+}
+
+const parseData = (record) => {
+  try {
+    const format = {
+      author_name: record.NickName,
+      id: generateId(record.observation.RegId),
+      timestamp: new Date(record.DtObsTime),
+      lat: record.Latitude,
+      long: record.Longitude,
+      depth: Number(record.Registrations[0].FullObject.SnowDepth),
+      source: "regObs"
+    };
+    if (isNaN(format.depth) || !format.depth) throw new Error("Snow Depth Undefined");
+    return format;
+  }
+  catch (error) {
+    return null;
+  }
+}
+
+module.exports = {
+  rawData,
+  parseData
+}
