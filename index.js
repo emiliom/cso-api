c6d0281194a1d39d111ad84f8e7f061467cd54ee
Jonah Joughin <jonah@jough.in>
2018-11-30 17:42:12 +0400

    Removed extra files

--- index.js
+++
@@ -1,7 +0,0 @@
-const getObservations = require('./src/api/v1.js')
-const fetchObservations = require('./src/import/index.js')
-
-module.exports = {
-  getObservations,
-  fetchObservations
-}
--- src/api/v1.js
+++
@@ -1,8 +0,0 @@
-const { parse } = require("url");
-const getObservations = require('./getObservations')
-
-module.exports = (req, res) => {
-  const queryParams = parse(req.url, true).query;
-  getObservations(queryParams)
-    .then(results => res.end(results))
-}
